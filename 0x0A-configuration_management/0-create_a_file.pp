c2d80d9d3031dacbeb79aae7432ba715abdf11d0
alazarwork <alazarworkneh@gmail.com>
2023-06-28 04:41:40 -0700

    m

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 
 file { 'holberton':
   mode    => '0744',
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
